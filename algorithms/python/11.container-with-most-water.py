083b1ca2eb82a26e930ac22fefee0086d0175705
Rylan.Loong <sangun.yong@gmail.com>
2021-03-18 18:53:07 +0800

    modify execution mode

--- algorithms/python/11.container-with-most-water.py
+++ algorithms/python/11.container-with-most-water.py
@@ -6,24 +6,16 @@
 
 # @lc code=start
 
-### These will be `Time Limit Exceeded` error by using double loop.
-
 # class Solution:
-
-#     def maxArea(self, height: List[int]) -> int:
-#         count = len(height)
-#         areaMax = 0
-#         for i in range(count):
-#             for j in range(i, count):
-#                 area = min(height[i], height[j]) * (j - i)
-#                 areaMax = max(areaMax, area)
-
-#         return areaMax
 
 
 class Solution:
 
     def maxArea(self, height: List[int]) -> int:
+        return self.usingTwoPointers(height)
+        # return self.bruteForce(height)  # timeout, to be optimized.
+
+    def usingTwoPointers(self, height):
         areaMax, head, tail = 0, 0, len(height) - 1
         while head < tail:
             area = min(height[head], height[tail]) * (tail - head)
@@ -32,6 +24,17 @@ class Solution:
                 head += 1
             else:
                 tail -= 1
+
+        return areaMax
+
+    def bruteForce(self, height):
+        # These will be `Time Limit Exceeded` error by using double loop.
+        count = len(height)
+        areaMax = 0
+        for i in range(count):
+            for j in range(i, count):
+                area = min(height[i], height[j]) * (j - i)
+                areaMax = max(areaMax, area)
 
         return areaMax
 
