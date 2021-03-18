d329d5aa8de382b23409e4069fac904c3523b043
Rylan.Loong <sangun.yong@gmail.com>
2021-03-18 18:36:31 +0800

    modify execution mode

--- algorithms/python/1.two-sum.py
+++ algorithms/python/1.two-sum.py
@@ -7,15 +7,20 @@
 # @lc code=start
 
 
-# Exhaustive method
 class Solution:
 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        return self.usingExhaustive(nums, target)
+
+    def usingExhaustive(self, nums, target):
         numsCount = len(nums)
         for i in range(numsCount):
             for j in range(i + 1, numsCount):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+    def usingHashMap(self, nums, target):
+        pass
 
 
 # hash method
