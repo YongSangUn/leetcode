2cb7ac5b011b1a07070cfa13b4fe28b29b82b5df
Rylan.Loong <sangun.yong@gmail.com>
2021-04-08 12:32:33 +0800

    add new Solution by awk

--- algorithms/shell/193.valid-phone-numbers.sh
+++ algorithms/shell/193.valid-phone-numbers.sh
@@ -7,6 +7,6 @@
 # @lc code=start
 # Read from the file file.txt and output all valid phone numbers to stdout.
 
-awk '$0 ~ /^([0-9]{3}-){2}[0-9]{4}$/ || $0 ~ /^\([0-9]{3}\) [0-9]{3}-[0-9]{4}$/ {print $0}' file.txt
+awk '/^([0-9]{3}-){2}[0-9]{4}$/ || /^\([0-9]{3}\) [0-9]{3}-[0-9]{4}$/' file.txt
 
 # @lc code=end
