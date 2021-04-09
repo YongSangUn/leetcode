37d736e9dff42d081c7b441706de9644ea9401bc
Rylan.Loong <sangun.yong@gmail.com>
2021-04-09 16:20:49 +0800

    modified to used function

--- algorithms/shell/192.word-frequency.sh
+++ algorithms/shell/192.word-frequency.sh
@@ -7,10 +7,14 @@
 # @lc code=start
 # Read from the file words.txt and output the word frequency list to stdout.
 
-sed 's/^ \+//g;s/ \+$//g;s/ \+/\n/g;/^$/d' words.txt |
-    sort |
-    uniq -c |
-    sort -nr |
-    awk '{print $2,$1}'
+function using_everything() {
+    sed 's/^ \+//g;s/ \+$//g;s/ \+/\n/g;/^$/d' words.txt |
+        sort |
+        uniq -c |
+        sort -nr |
+        awk '{print $2,$1}'
+}
+
+using_everything
 
 # @lc code=end
